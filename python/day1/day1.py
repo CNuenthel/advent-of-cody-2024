7799a2a9d7a9fd00b387294553f5af3bfc0567b6
Cody Nuenthel <89996557+CNuenthel@users.noreply.github.com>
2024-12-06 21:31:11 -0600

    day1
    Decided to try and solve with recursion to speed up list iteration as I went. Turns out input size was ~1000 and had to increase recursion limit, not because my code failed (for the first time ever) but because I was going deeeeeeeeeep.

---
+++ python/day1/day1.py
@@ -0,0 +1,37 @@
+import sys
+
+sys.setrecursionlimit(1025)
+
+with open("day1_input.txt", "r") as f:
+    data = f.read()
+
+rows = data.splitlines()
+
+left = []
+right = []
+calcs = []
+
+for item in rows:
+    nums = item.split("   ")
+    left.append(int(nums[0]))
+    right.append(int(nums[1]))
+
+def recursive_subtract(left_list, right_list):
+    if not left_list and not right_list:
+        return
+
+    num1 = min(left_list)
+    num2 = min(right_list)
+    calcs.append(abs(num1-num2))
+
+    left_list.remove(num1)
+    right_list.remove(num2)
+
+    recursive_subtract(left_list, right_list)
+
+
+recursive_subtract(left, right)
+
+print(sum(calcs))
+
+
