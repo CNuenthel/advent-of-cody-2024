6cf003e6901f6f8dfc2894d6aaddf12d7b4f9260
Cody Nuenthel <89996557+CNuenthel@users.noreply.github.com>
2024-12-08 00:54:33 -0600

    Update day5a.py

--- python/day5/day5a.py
+++ python/day5/day5a.py
@@ -1,3 +1,23 @@
+"""
+If you set a dictionary value to 0 then check for the existance of the value directly, it may return unintended results. 
+i.e:
+dict = {"9": 0}
+
+Where 0 is Falsey, but in this case it represents an index where the str value "9" is in a list. If we check if the key "9" has a value like this:
+
+if dict.get("9"):
+    ...
+
+if dict.get("9") will return False, since this translates to < if 0 > which is interpreted as false, since 0 is falsey.
+
+This is demonstrated by
+if 0:
+    print("OK")
+
+So ... uh... if you ever set a key to a value of 0, watch out for this. 
+
+"""
+
 
 def applicable_pages(rulebook: dict, present_pages: list, rule_target: str):
     ruled_pages = []
